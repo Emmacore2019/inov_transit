2716df2d62ea948d880519b400fb7c5ca4c2bcb1
EMMANUEL MOUSSI <emmanuel.moussipee@fgeservice.com>
2025-07-24 22:28:37 +0100

    ok

--- models/task_check_list.py
+++ models/task_check_list.py
@@ -14,6 +14,10 @@ class TaskChecklist(models.Model):
         'res.users',
         string='Poste',
     )
+    stage_id = fields.Many2one(
+        'stages.transit',
+        string='Etapes',
+        )
 
 
 class TaskActivityTransit(models.Model):
@@ -52,61 +56,6 @@ class TaskActivityTransit(models.Model):
         )._action_done(feedback=feedback, attachment_ids=attachment_ids)
         return messages[0].id if messages else False
        
-      
-    # def action_feedback(self, feedback=False):
-    #     message = self.env['mail.message']
-    #     if feedback:
-    #         self.write(dict(feedback=feedback))
-    #     for activity in self:
-    #         if activity.user_id != self.env.user:
-    #             raise ValidationError(
-    #                 _("L'utilisateur %s n'a pas le droit de confirmer cette Tache, seul %s Doit la validée, Merci!") % (
-    #                 self.env.user.name, activity.user_id.name))
-    #         record = self.env[activity.res_model].browse(activity.res_id)
-    #         record.message_post(
-    #             'mail.message_activity_done',
-    #             values={'activity': activity},
-    #             subtype_id=self.env['ir.model.data']._xmlid_to_res_id('mail.mt_activities'),
-    #             mail_activity_type_id=activity.activity_type_id.id,
-    #         )
-    #         message |= record.message_ids[0]
-    #         vals = {
-    #             'name': activity.activity_type_id.name,
-    #             'responsible_id': activity.user_id.name,
-    #             'date_start': activity.date_deadline,
-    #             'date_dealine': fields.Date.today(),
-    #             'folder_id': activity.res_id,
-    #         }
-    #         self.env['task.checklist'].create(vals)
-    #         self.unlink()
-    #     return message.ids and message.ids[0] or False
-    # def action_feedback(self, feedback=False):
-    #     message = self.env['mail.message']
-    #     if feedback:
-    #         self.write(dict(feedback=feedback))
-    #     for activity in self:
-    #         if activity.user_id != self.env.user:
-    #             raise ValidationError(
-    #                 _("L'utilisateur %s n'a pas le droit de confirmer cette Tache, seul %s Doit la validée, Merci!") % (
-    #                 self.env.user.name, activity.user_id.name))
-    #         record = self.env[activity.res_model].browse(activity.res_id)
-    #         record.message_post_with_view(
-    #             'mail.message_activity_done',
-    #             values={'activity': activity},
-    #             subtype_id=self.env['ir.model.data'].xmlid_to_res_id('mail.mt_activities'),
-    #             mail_activity_type_id=activity.activity_type_id.id,
-    #         )
-    #         message |= record.message_ids[0]
-    #         vals = {
-    #             'name': activity.activity_type_id.name,
-    #             'responsible_id': activity.user_id.name,
-    #             'date_start': activity.date_deadline,
-    #             'date_dealine': fields.Date.today(),
-    #             'folder_id': activity.res_id,
-    #         }
-    #         self.env['task.checklist'].create(vals)
-    #         self.unlink()
-    #     return message.ids and message.ids[0] or False
 class TaskTransitChecklist(models.Model):
     _name = 'task.checklist'
     _description = 'Checklist for the task'
@@ -119,5 +68,29 @@ class TaskTransitChecklist(models.Model):
     date_dealine = fields.Date("Date Validation", required=True)
     date_start = fields.Date("Date Creation de la tache", required=True)
     folder_id = fields.Many2one("folder.transit", string="Dossier")
+    
+    time_spent_days = fields.Integer(
+        string="Temps passé (jours)",
+        compute="_compute_time_spent",
+        store=True
+    )
+    time_spent_float = fields.Float(
+        string="Temps passé (heures décimales)",
+        compute="_compute_time_spent",
+        store=True
+    )
+    
+    state = fields.Selection([('transit', 'Dedouanement'), ('accone', 'Acconage'), ('ship', 'Shipping')], string='Type de dossier', related="folder_id.stages",store=True)
+    
+    @api.depends('date_start', 'date_dealine')
+    def _compute_time_spent(self):
+        for rec in self:
+            if rec.date_start and rec.date_dealine:
+                delta = rec.date_dealine - rec.date_start
+                rec.time_spent_days = delta.days
+                rec.time_spent_float = delta.seconds
+            else:
+                rec.time_spent_days = 0
+                rec.time_spent_float = 0.0
 
 
